57ba20133d00eb9fb7108f0a51727ef73bf5d393
novivinop <nnovi452@gmail.com>
2022-02-04 21:43:20 +0700

    praktikum 9

---
+++ Pertemuan9/app/src/main/java/id/novi_19104079/pertemuan9/SettingModel.kt
@@ -0,0 +1,12 @@
+package id.novi_19104079.pertemuan9
+
+import android.os.Parcelable
+
+@Parcelize
+data class SettingModel(
+    var name: String? = null,
+    var email: String? = null,
+    var age: Int = 0,
+    var phoneNumber: String? = null,
+    var isDarkTheme: Boolean = false
+) : Parcelable
